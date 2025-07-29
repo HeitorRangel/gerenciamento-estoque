a958d8102ce45e1584269c5a156a118d75fa63fc
HeitorRangel <heitorespinosa@gmail.com>
2025-07-29 15:39:43 -0300

    feat(spring)Prepara Ambiente e instala dependências

---
+++ gerenciadorapp/src/main/java/br/edu/iff/ccc/gerenciadorapp/GerenciadorappApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.gerenciadorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorappApplication.class, args);
+	}
+
+}
---
+++ gerenciadorapp/src/test/java/br/edu/iff/ccc/gerenciadorapp/GerenciadorappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.gerenciadorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
