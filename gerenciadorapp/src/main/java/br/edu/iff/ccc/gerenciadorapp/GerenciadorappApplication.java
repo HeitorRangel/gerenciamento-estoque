3600b6756d24d0e141df97b280ab1bf9b03f22e5
HeitorRangel <heitorespinosa@gmail.com>
2025-07-29 16:04:26 -0300

    feat(home)Cria da página home e do MainViewController

--- gerenciadorapp/src/main/java/br/edu/iff/ccc/gerenciadorapp/GerenciadorappApplication.java
+++ gerenciadorapp/src/main/java/br/edu/iff/ccc/gerenciadorapp/GerenciadorappApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GerenciadorappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorappApplication.class, args);
 	}
---
+++ gerenciadorapp/src/main/java/br/edu/iff/ccc/gerenciadorapp/controller/view/MainViewController.java
@@ -0,0 +1,16 @@
+package br.edu.iff.ccc.gerenciadorapp.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(path = "/principal")
+public class MainViewController {
+
+    @GetMapping()
+    public String getMethodName() {
+        return "home.html";
+    }    
+}
